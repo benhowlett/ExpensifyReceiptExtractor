65d0322f2c855f78b9838052dfd50db4260d79bd
Ben Howlett <ben@benhowlett.com>
2023-09-25 10:19:37 -0600

    Added window and event loop

--- ExpensifyReceiptExtractor.py
+++ ExpensifyReceiptExtractor.py
@@ -30,7 +30,8 @@ class Expense:
                 return "Incompatible File Type"
 
 
-main_column = [
+# Create the layout as a single column
+layout = [
     [
         sg.Text("Welcome to the TAG Expensify Receipt Extractor.\nPlease follow the instructions below to extract receipts from Expensify exports."),
         sg.Push(),
@@ -55,57 +56,68 @@ main_column = [
     ]
 ]
 
+# Create the window
+window = sg.Window("Expensify Receipt Extractor", layout)
 
-# Create a list of Expense instances from the expense .csv file(s) in the 'Expense Exports' folder
-expenses = []
-
-for file in glob("Expense Exports/*.csv"):
-    with open(file) as csvfile:
-        fileReader = csv.reader(csvfile, delimiter=',', quotechar='"')
-        firstRow = True
-        for row in fileReader:
-            if not firstRow:
-                name = row[27][0:row[27].find('@')].replace('.', ' ').title()
-                customer = row[11][row[11].find(":")+1:len(row[11])]
-                expenses.append(Expense(row[1], customer, name, row[53]))
-            else:
-                firstRow = False
-
-# Download the receipts from the list of Expense instances. Sort PDFs from JPGs and name appropriately
-fileNames = []
-
-for expense in expenses:
-    if not expense.url == "":
-        fileName = expense.name + "_" + expense.customer + "_" + expense.merchant
-        suffix = fileNames.count(fileName) + 1
-        fileNames.append(fileName)
-        extension = expense.get_content_file_extension()
-        if extension == ".pdf":
-            filePath = 'PDFs/' + fileName + "_" + str(suffix) + extension
-            request.urlretrieve(expense.url, filePath)
-            print ("Receipt saved: " + filePath)
-        elif extension == ".jpg":
-            filePath = 'Images/' + fileName + "_" + str(suffix) + extension
-            request.urlretrieve(expense.url, filePath)
-            print ("Image saved: " + filePath)
-    else:
-        print ("Expense does not have a receipt")
+# Run the event loop
+while True:
+	event, values = window.read()
+	if event in ("Exit", sg.WIN_CLOSED):
+		break
+	
+	
 
 
-# Convert images to PDFs. Remove the image files 
-for image in glob("Images/*.jpg"):
-    # Deal with Windows path formatting
-    image = image.replace("\\", "/")
-    fileName = image[image.find('/')+1:len(image)-4] + ".pdf"
-
-    # Open the image and convert to PDF
-    newFile = Image.open(image)
-    newFile.convert('RGB')
-    newFile.save('PDFs/' + fileName)
-    print ("Receipt saved from image: " + fileName)
-
-    # Delete the image file
-    if os.path.isfile(image):
-        os.remove(image)
-
-print ("Receipt extraction complete.")
+# # Create a list of Expense instances from the expense .csv file(s) in the 'Expense Exports' folder
+# expenses = []
+# 
+# for file in glob("Expense Exports/*.csv"):
+#     with open(file) as csvfile:
+#         fileReader = csv.reader(csvfile, delimiter=',', quotechar='"')
+#         firstRow = True
+#         for row in fileReader:
+#             if not firstRow:
+#                 name = row[27][0:row[27].find('@')].replace('.', ' ').title()
+#                 customer = row[11][row[11].find(":")+1:len(row[11])]
+#                 expenses.append(Expense(row[1], customer, name, row[53]))
+#             else:
+#                 firstRow = False
+# 
+# # Download the receipts from the list of Expense instances. Sort PDFs from JPGs and name appropriately
+# fileNames = []
+# 
+# for expense in expenses:
+#     if not expense.url == "":
+#         fileName = expense.name + "_" + expense.customer + "_" + expense.merchant
+#         suffix = fileNames.count(fileName) + 1
+#         fileNames.append(fileName)
+#         extension = expense.get_content_file_extension()
+#         if extension == ".pdf":
+#             filePath = 'PDFs/' + fileName + "_" + str(suffix) + extension
+#             request.urlretrieve(expense.url, filePath)
+#             print ("Receipt saved: " + filePath)
+#         elif extension == ".jpg":
+#             filePath = 'Images/' + fileName + "_" + str(suffix) + extension
+#             request.urlretrieve(expense.url, filePath)
+#             print ("Image saved: " + filePath)
+#     else:
+#         print ("Expense does not have a receipt")
+# 
+# 
+# # Convert images to PDFs. Remove the image files 
+# for image in glob("Images/*.jpg"):
+#     # Deal with Windows path formatting
+#     image = image.replace("\\", "/")
+#     fileName = image[image.find('/')+1:len(image)-4] + ".pdf"
+# 
+#     # Open the image and convert to PDF
+#     newFile = Image.open(image)
+#     newFile.convert('RGB')
+#     newFile.save('PDFs/' + fileName)
+#     print ("Receipt saved from image: " + fileName)
+# 
+#     # Delete the image file
+#     if os.path.isfile(image):
+#         os.remove(image)
+# 
+# print ("Receipt extraction complete.")
