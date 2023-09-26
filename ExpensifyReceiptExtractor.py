3798e1a5e31437477ecd5d1357eefb5e44cd420d
Ben Howlett <ben@benhowlett.com>
2023-09-26 13:05:55 -0600

    Hooked up the buttons to their functions

--- ExpensifyReceiptExtractor.py
+++ ExpensifyReceiptExtractor.py
@@ -30,6 +30,10 @@ class Expense:
                 return "Incompatible File Type"
 
 
+# Variables for managing UI
+files = []
+export_folder = ""
+
 # Create the layout as a single column
 layout = [
     [
@@ -43,7 +47,7 @@ layout = [
     ],
     [
         sg.Listbox(
-            values=[], enable_events=True, size=(60, 5), key="-REPORT LIST-"
+            files, enable_events=True, size=(60, 5), key="-REPORT LIST-"
         )
     ],
     [
@@ -51,12 +55,13 @@ layout = [
     ],
     [    
         sg.In(size=(53, 1), enable_events=True, key="-EXPORT FOLDER-"),
-        sg.FolderBrowse(target="-EXPORT FOLDER-")
+        sg.FolderBrowse(target="-EXPORT FOLDER-", key="-SELECT FOLDER-")
     ],
     [
-        sg.Button("Export Expense Receipts")
+        sg.Button("Export Expense Receipts", key="-EXPORT-")
     ]
 ]
+
 
 # Create the window
 window = sg.Window("Expensify Receipt Extractor", layout)
@@ -64,60 +69,76 @@ window = sg.Window("Expensify Receipt Extractor", layout)
 # Run the event loop
 while True:
     event, values = window.read()
-    if event in ("Exit", sg.WIN_CLOSED):
-	    break
+    print(event)
+    # End loop if the window is closed
+    if event == sg.WIN_CLOSED:
+        break
 
+    # Get list of files from -SELECT FILES- and populate the -REPORT LIST_
+    if event == "-REPORT LIST-":
+        files = values["-SELECT FILES-"].split(";")
+        file_names = []
+        for file in files:
+            name = os.path.basename(file)
+            file_names.append(name)
+        window["-REPORT LIST-"].update(file_names)
 
-# # Create a list of Expense instances from the expense .csv file(s) in the 'Expense Exports' folder
-# expenses = []
-# 
-# for file in glob("Expense Exports/*.csv"):
-#     with open(file) as csvfile:
-#         fileReader = csv.reader(csvfile, delimiter=',', quotechar='"')
-#         firstRow = True
-#         for row in fileReader:
-#             if not firstRow:
-#                 name = row[27][0:row[27].find('@')].replace('.', ' ').title()
-#                 customer = row[11][row[11].find(":")+1:len(row[11])]
-#                 expenses.append(Expense(row[1], customer, name, row[53]))
-#             else:
-#                 firstRow = False
-# 
-# # Download the receipts from the list of Expense instances. Sort PDFs from JPGs and name appropriately
-# fileNames = []
-# 
-# for expense in expenses:
-#     if not expense.url == "":
-#         fileName = expense.name + "_" + expense.customer + "_" + expense.merchant
-#         suffix = fileNames.count(fileName) + 1
-#         fileNames.append(fileName)
-#         extension = expense.get_content_file_extension()
-#         if extension == ".pdf":
-#             filePath = 'PDFs/' + fileName + "_" + str(suffix) + extension
-#             request.urlretrieve(expense.url, filePath)
-#             print ("Receipt saved: " + filePath)
-#         elif extension == ".jpg":
-#             filePath = 'Images/' + fileName + "_" + str(suffix) + extension
-#             request.urlretrieve(expense.url, filePath)
-#             print ("Image saved: " + filePath)
-#     else:
-#         print ("Expense does not have a receipt")
-# 
-# 
-# # Convert images to PDFs. Remove the image files 
-# for image in glob("Images/*.jpg"):
-#     # Deal with Windows path formatting
-#     image = image.replace("\\", "/")
-#     fileName = image[image.find('/')+1:len(image)-4] + ".pdf"
-# 
-#     # Open the image and convert to PDF
-#     newFile = Image.open(image)
-#     newFile.convert('RGB')
-#     newFile.save('PDFs/' + fileName)
-#     print ("Receipt saved from image: " + fileName)
-# 
-#     # Delete the image file
-#     if os.path.isfile(image):
-#         os.remove(image)
-# 
-# print ("Receipt extraction complete.")
+    if event == "-EXPORT FOLDER-":
+        export_folder = values["-SELECT FOLDER-"]
+
+    if event == "-EXPORT-":
+        # Create a list of Expense objects from the files that were selected
+        expenses = []
+
+        for file in files:
+            with open(file) as csvfile:
+                fileReader = csv.reader(csvfile, delimiter=',', quotechar='"')
+                firstRow = True
+                for row in fileReader:
+                    if not firstRow:
+                        name = row[27][0:row[27].find('@')].replace('.', ' ').title()
+                        customer = row[11][row[11].find(":")+1:len(row[11])]
+                        expenses.append(Expense(row[1], customer, name, row[53]))
+                    else:
+                        firstRow = False
+        
+        # Download the receipts from the list of Expense instances. Sort PDFs from JPGs and name appropriately
+        fileNames = []
+        os.mkdir(export_folder + "/Images")
+
+        for expense in expenses:
+            if not expense.url == "":
+                fileName = expense.name + "_" + expense.customer + "_" + expense.merchant
+                suffix = fileNames.count(fileName) + 1
+                fileNames.append(fileName)
+                extension = expense.get_content_file_extension()
+                if extension == ".pdf":
+                    filePath = export_folder + "/" + fileName + "_" + str(suffix) + extension
+                    request.urlretrieve(expense.url, filePath)
+                    # print ("Receipt saved: " + filePath)
+                elif extension == ".jpg":
+                    filePath = export_folder + '/Images/' + fileName + "_" + str(suffix) + extension
+                    request.urlretrieve(expense.url, filePath)
+                    # print ("Image saved: " + filePath)
+            else:
+                print ("Expense does not have a receipt")
+
+        # Convert images to PDFs. Remove the image files 
+        for image in glob(export_folder + "/Images/*.jpg"):
+            # Deal with Windows path formatting
+            image = image.replace("\\", "/")
+            fileName = os.path.basename(image)
+            fileName = fileName.replace("jpg", "pdf")
+
+            # Open the image and convert to PDF
+            newFile = Image.open(image)
+            newFile.convert('RGB')
+            newFile.save(export_folder + "/" + fileName)
+            # print ("Receipt saved from image: " + fileName)
+
+            # Delete the image file
+            if os.path.isfile(image):
+                os.remove(image)
+        
+        # Remove the Images directory
+        os.rmdir(export_folder + "/Images")
